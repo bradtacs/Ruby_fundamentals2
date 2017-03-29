4573b85496e5b3559dea7334141c1058b2548b6b
Zachary Viggiani <zach@bigmojo.net>
2017-03-29 17:22:42 -0400

    got loop of items working.

---
+++ excercise9.rb
@@ -0,0 +1,5 @@
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each  { |x|  } # this is using x to interate through the numnbers and added them up l
+puts " pp" puts grocery_list 
