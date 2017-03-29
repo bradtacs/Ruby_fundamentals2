d71ff7e0490ad19015ce66a498b99d876a281884
Zachary Viggiani <zach@bigmojo.net>
2017-03-29 19:14:33 -0400

    Number two done

--- excercise9.rb
+++ excercise9.rb
@@ -1,5 +1,17 @@
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list  = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each  { |x|  } # this is using x to interate through the numnbers and added them up l
-puts " pp" puts grocery_list 
+#grocery_list.each  { |x|   } # this is using x to interate through the numnbers and added them up l
+
+grocery_list << "rice" # puts rice in
+
+puts "Number of items in your list "
+puts grocery_list.length
+
+def star_items(grocery_list)
+    grocery_list.each  { |x|  puts "*" + x   }
+
+
+end
+
+star_items(grocery_list)
