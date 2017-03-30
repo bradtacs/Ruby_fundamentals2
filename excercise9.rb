3097315076b2ba6f52fc9a2398df1bfa62fe3f52
Zachary Viggiani <zach@bigmojo.net>
2017-03-30 09:01:30 -0400

    everything is done

--- excercise9.rb
+++ excercise9.rb
@@ -8,10 +8,24 @@ grocery_list << "rice" # puts rice in
 puts "Number of items in your list "
 puts grocery_list.length
 
+puts "2nd item in my list"
+puts grocery_list[1]
+
 def star_items(grocery_list)
-    grocery_list.each  { |x|  puts "*" + x   }
+  bannana_exist = 0
+    grocery_list.each  do  |x| # this is using x to interate through the numnbers and check them  l
 
 
+      puts "*" + x
+      if x == "bannana"
+        bannana_exist = 1
+       end
+
+    end
+    if bannana_exist == 0
+      puts "Add more bannans"
+
+    end
 end
 
 star_items(grocery_list)
