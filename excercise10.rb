af95c2739ea41664f1b3528b11233da4413437ef
Zachary Viggiani <zach@bigmojo.net>
2017-03-30 14:09:18 -0400

    number 3 works

--- excercise10.rb
+++ excercise10.rb
@@ -2,10 +2,13 @@
 students = {
   :cohort  => 34,
   :cohort2 => 42,
-  :cohort3 => 22
+  :cohort3 => 22,
 }
 
+# add to hash
+students["cohort4"] = 43
+
 # as long as the numbers letters match it should plug them right in
-students.each do |x, v|
-  puts "#{x} #{v} students"
+students.each do |k, v|
+  puts "#{k} #{v} students"
 end
